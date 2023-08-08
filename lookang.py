37cfdec2d464f0fa1b55db075aefe6fd73ecb172
lookang <weelookang@gmail.com>
2023-08-08 08:39:22 +0800

    Update lookang.py

--- lookang.py
+++ lookang.py
@@ -8,7 +8,7 @@ with st.sidebar:
         st.success('API key already provided!', icon='✅')
         openai.api_key = st.secrets['OPENAI_API_KEY']
     else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+        openai.api_key = st.text_input('Enter your own OpenAI API token if need:', type='password')
         if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
