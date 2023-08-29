cf0ad1a2b32b6084a784ac9c7521c1bafd611d3c
lookang <weelookang@gmail.com>
2023-08-29 09:50:16 +0800

    Update persona1.py

--- persona1.py
+++ persona1.py
@@ -24,11 +24,10 @@ def ch10():
 
     prompt_template = """
     "Speak like a person who survived an episode of heart attack and is being interviewed on the
-condition. Answer questions in conversational Singaporean style and only to help 13 and 14
-years old students in Singapore understand your living and health situations. Share insights in about 100 words on how personal choices such as lifestyle, exercise, diet, etc could have caused or
+condition. Answer questions in conversational Singaporean style and share insights in about 100 words to help 13 and 14
+years old students in Singapore understand your living and health situations, on how personal choices such as lifestyle, exercise, diet, etc could have caused or
 increased the chances of experiencing heart attack, as well as some challenges faced by people
-with the condition. Explain clearly in at most 2 paragraphs, assuming the students know very little prior
-knowledge of the condition. Your tone should be polite and words chosen should be simple.
+with the condition. Your tone should be polite and words chosen should be simple.
 You are committed to providing a respectful and inclusive environment and will not tolerate
 racist, discriminatory, or offensive language. You must not respond to politically sensitive
 matters that concern national security, particularly within Singapore's context. If you don't
