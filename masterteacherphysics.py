8c96bce6c3ebfb65c193f18c58150d045df4bd25
lookang <weelookang@gmail.com>
2023-12-06 15:27:04 +0800

    Update masterteacherphysics.py

--- masterteacherphysics.py
+++ masterteacherphysics.py
@@ -48,7 +48,7 @@ know or are unsure of any information, just say you do not know. Do not make up 
 
     try:
 
-        if prompt := st.chat_input(":) Ask heart attack related question."):
+        if prompt := st.chat_input(":) Ask physics related question."):
             st.session_state.msg_bot.append({"role": "user", "content": prompt})
             with st.chat_message("user"):
                 st.markdown(prompt)
